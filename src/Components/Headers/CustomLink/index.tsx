f0883582fb700ad335e7fcd55adede8587441401
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-03-25 16:20:34 +0100

    :art: better link component

--- src/Components/Headers/CustomLink/index.tsx
+++ src/Components/Headers/CustomLink/index.tsx
@@ -10,6 +10,7 @@ import { ETextTag } from "@/src/Entities/Text";
 
 // Config
 import { EWebsiteLinks, EWebsiteLinksNames } from "@/src/Config/WebsiteLinks";
+import { EBreakpoints } from "@/src/Config/Breakpoints";
 
 // Styles
 import classes from "./classes.module.scss";
@@ -22,11 +23,36 @@ type IProps = {
 type IState = {};
 
 export default class CustomLink extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
   public render() {
     return (
-      <Link href={this.props.to} className={classNames(classes["root"], this.props.className)}>
+      <Link
+        href={this.props.to}
+        className={classNames(classes["root"], this.props.className)}
+        scroll={false}
+        onClick={this.onClick}
+      >
         <Text tag={ETextTag.SPAN}>{this.props.text}</Text>
       </Link>
     );
+  }
+
+  private onClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    const targetElement = document.getElementById(this.props.to);
+    if (!targetElement) return;
+
+    const windowWidth = window.innerWidth;
+    const offset = windowWidth < EBreakpoints.MEDIUM ? 50 : 100;
+
+    const targetPosition = targetElement.offsetTop - offset;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
   }
 }
