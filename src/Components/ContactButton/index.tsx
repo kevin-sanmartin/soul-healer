83ba5be9006567d67354d3d38fd00e53666ca457
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-05-01 13:16:55 +0200

    :sparkles: add contact button component

---
+++ src/Components/ContactButton/index.tsx
@@ -0,0 +1,49 @@
+import { Component } from "react";
+import { PopupButton } from "react-calendly";
+
+// Entities
+import { EButtonSize } from "@/src/Entities/Button";
+
+// Styles
+import classes from "./classes.module.scss";
+import { ZCOOL_XiaoWei } from "@next/font/google";
+const xiaoWei = ZCOOL_XiaoWei({ weight: ["400"], subsets: ["latin"], display: "swap" });
+
+type IProps = {
+  size: EButtonSize;
+};
+type IState = {
+  rootElement: HTMLElement | null;
+};
+
+export default class ContactButton extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      rootElement: null,
+    };
+  }
+
+  override render() {
+    if (!this.state.rootElement) return null;
+    return (
+      <PopupButton
+        url="https://calendly.com/kevinsanmartin/30min"
+        rootElement={this.state.rootElement}
+        styles={{ fontFamily: xiaoWei.style.fontFamily }}
+        className={classes["root"]}
+        text="CONTACTEZ-MOI"
+      />
+    );
+  }
+
+  public componentDidMount() {
+    this.setState({ rootElement: document.getElementById("__next") });
+  }
+
+  public componentDidUpdate(_: Readonly<IProps>, prevState: Readonly<IState>): void {
+    if (!this.state.rootElement && prevState.rootElement !== this.state.rootElement) {
+      this.setState({ rootElement: document.getElementById("__next") });
+    }
+  }
+}
