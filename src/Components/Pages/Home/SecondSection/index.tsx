30e89efd8c0cff40a68fe2d05c6808fcbb564383
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-05-18 17:31:30 +0200

    :hammer: test thumbnail video

--- src/Components/Pages/Home/SecondSection/index.tsx
+++ src/Components/Pages/Home/SecondSection/index.tsx
@@ -46,13 +46,8 @@ export default class SecondSection extends Component<IProps, IState> {
               [classes["is-visible"]]: this.state.isPresentationContainerVisible,
             })}
           >
-            <video
-              controls
-              controlsList="nodownload"
-              className={classes["video"]}
-              playsInline
-              poster={"/images/outside/portrait-professionnel-046-min.jpg"}
-            >
+            <video controls controlsList="nodownload" className={classes["video"]} playsInline>
+              <img src="public/images/outside/portrait-professionnel-034-min.jpg" alt="Portrait professionnel" />
               <source src="/presentation-video.mp4" type="video/mp4" />
             </video>
           </div>
