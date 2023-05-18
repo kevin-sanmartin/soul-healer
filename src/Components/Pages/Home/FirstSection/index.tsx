eaefd94e0766ca9fb8999aeb3cd4b43350a18034
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-05-18 16:38:10 +0200

    :sparkles: add name in description of first section

--- src/Components/Pages/Home/FirstSection/index.tsx
+++ src/Components/Pages/Home/FirstSection/index.tsx
@@ -30,8 +30,8 @@ export default class FirstSection extends Component<IProps, IState> {
           </Text>
 
           <Text tag={ETextTag.P} className={classes["description"]}>
-            Si vous cherchez une complémentarité a la médecine traditionnelle, sachez que vous pouvez prendre votre
-            santé en main et vous soigner vous-même.
+            Je m&apos;appelle Nadia Richard, l&apos;âme guérisseuse, si vous cherchez une complémentarité a la médecine
+            traditionnelle, sachez que vous pouvez prendre votre santé en main et vous soigner vous-même.
           </Text>
 
           <ContactButton size={EButtonSize.LARGE} />
