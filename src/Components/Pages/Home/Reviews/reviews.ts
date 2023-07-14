903e59e381c24580b54526ab1149d452acc13473
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-07-14 19:47:15 +0200

    :sparkles: add one review

--- src/Components/Pages/Home/Reviews/reviews.ts
+++ src/Components/Pages/Home/Reviews/reviews.ts
@@ -17,27 +17,34 @@ export const reviews: IReview[] = [
   },
   {
     id: 3,
+    name: "Stepha Winona Bardi",
+    review:
+      "J'ai eu un abcès dentaire et aussi sur la gencive qui m'a atrocement fait mal insupportable même malgré l'huile de clou de girofle les bains de bouche au chlorure de magnésium ou même à l'argent colloïdal alors j'ai fait appel à Nadia.. et à distance elle a su soulager ma douleur j'ai ressenti comme une sensation intense de fraîcheur et d'apaisement de la douleur, la douleur est revenue le lendemain c'est vrai mais quel bonheur d'avoir pu dormir car cela faisait 4 nuits que je ne dormais plus , puis tu as recommencé le soin à distance et là pareil j'ai ressenti la même chose puis plus de douleur alors je tiens à te remercier et je te recommanderai avec tout mon cœur. Sois béni pour tout l'amour que tu m'as apporté.",
+    sessionType: "Séance à distance",
+  },
+  {
+    id: 4,
     name: "Mégane Guisnet",
     review:
       "Nadia est une personne douce et bienveillante qui mets du coeur dans tout ce qu'elle fait. Elle nous accueil à son domicile où on se sent tout de suite très bien et à l'aise. La pièce dans laquelle elle nous accueil est décoré avec goût et est un espace calme. J'ai pu bénéficier de ces soins pour divers maux: douleurs lombaires, sinusite et nous travaillons encore sur mes problèmes de digestion ( colon irritable ). Il suffit de se laisser aller, de se détendre et de laisser opérer ses mains chaudes. Ces séances font l'effet d'un apaisement considérable ! Nadia explique le déroulé, donne tous les conseils sur l'après séance et adopte un réel suivi. Suite aux séances les douleurs ont  disparus! Je recommande sans aucun doute pour son peofessionalisme , son humanisme et sa douceur. Merci Nadia !",
     sessionType: "Séance à domicile",
   },
   {
-    id: 4,
+    id: 5,
     name: "Laura De jesus",
     review:
       "Nadia est une personne incroyable ! Elle est très professionnelle et à l'écoute. Je suis une personne qui a du mal à faire confiance et à croire en ces énergie mais je me suis laissée aller et je ne le regrette pas ! J'ai pu avoir l'opportunité de faire plusieurs soins avec elle notamment pour des maux de dos, ainsi que pour d'autres problèmes de santé. Je peux alors vous affirmer que cela fonctionne et que l'on voit les résultats directement. Je vous invite alors à lui faire confiance et à vous laisser aller.",
     sessionType: "Séance à domicile",
   },
   {
-    id: 5,
+    id: 6,
     name: "Aurélie Beaufay",
     review:
       "Je connaissais Nadia en tant qu'assistante maternelle, et je lui parlais depuis quelques temps d'une douleur persistante à la hanche. Etant assez ouverte à ce genre de médecine alternative, j'ai décidé de tenter l'expérience ! Nadia a pris le temps de me mettre en conditions lors du soin, de chercher avec moi l'origine de cette douleur. Beaucoup de bienveillance et de douceur. Le soin en lui même est un moment hors du temps, ou émotions et ressentis se mêlent. La chaleur de ses mains, la sensation de picotements sur la tête, avec l'impression que quelque chose partait par les cheveux ! Assez incroyable. Cela fait maintenant 1 année que cette douleur s'est échapée. Je ne peux que vous conseiller de rencontrer cette belle personne !",
     sessionType: "Séance à domicile",
   },
   {
-    id: 6,
+    id: 7,
     name: "Laurans Magali",
     review:
       "Je suis allée consulter nadia à cause d'une douleur dans le milieu du dos persistante depuis longtemps. J'ai été installé dans une pièce au calme et joliment décorée et équipée d'une table de massage confortable.  L'endroit été très agréable. Nadia est douce . Elle m'a manipulé avec beaucoup de douceur et une chaleur s'est ressentie dans mos dos au fur et à mesure du soin. Je me suis presque endormie! A la fin du soin je n'ai pas sentie immédiatement le soulagement mais le lendemain la douleur s'était déplacée dans le bas du dos. 2 jours après j'ai vraiment senti une nette amélioration  je n'ai plus de douleur permanente ,seulement parfois quand je maintient une position trop longtemps. Mais le bienfait du soin de nadia est indéniable.  Je recommande chaudement ses soins apaisants et efficace.",
