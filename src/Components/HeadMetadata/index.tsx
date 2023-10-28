19c76237d215583655fcc3aa188d887325ade9a0
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-10-28 14:14:27 +0200

    :art: metadata

--- src/Components/HeadMetadata/index.tsx
+++ src/Components/HeadMetadata/index.tsx
@@ -16,7 +16,7 @@ export default class HeadMetadata extends Component<IProps, IState> {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
         <meta name="author" content="Nadia Richard" />
-        <meta name="canonical" content="https://l-ame-guerrisseuse.fr/" />
+        <meta name="canonical" content="https://www.nadia-richard.fr" />
 
         <meta property="og:title" content="Soins énergétique à domicile ou à distance - Nadia Richard" />
         <meta
@@ -24,14 +24,14 @@ export default class HeadMetadata extends Component<IProps, IState> {
           content="soins énergétiques, bien-être, stress, anxiété, douleur, équilibre, thérapie, harmonie, méditation, énergéticienne"
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://l-ame-guerrisseuse.fr/" />
+        <meta property="og:url" content="https://www.nadia-richard.fr/" />
         <meta
           property="og:description"
           content="Retrouvez un équilibre énergétique optimal grâce à mes soins. Soulagez le stress et l'anxiété. Sur Grasse dans les Alpes-maritimes. À distance ou chez moi."
         />
         <meta
           property="og:image"
-          content="https://l-ame-guerrisseuse.fr/images/healingSeance/portrait-professionnel-021-min.jpg"
+          content="https://www.nadia-richard.fr/images/healingSeance/portrait-professionnel-021-min.jpg"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
