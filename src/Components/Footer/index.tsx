4eb6d7dafcc698a4b6d8cac6ac041330d298f054
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2024-02-03 18:45:53 +0100

    :art: no referrer on social media links

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -19,10 +19,10 @@ export default class Footer extends Component<IProps, IState> {
       <footer className={classes["root"]}>
         <LegalNotice />
         <div className={classes["social-media-container"]}>
-          <a href="https://www.instagram.com/nadia.richard06" target="_blank">
+          <a href="https://www.instagram.com/nadia.richard06" target="_blank" rel="noreferrer">
             <FaInstagram className={classes["icon"]} />
           </a>
-          <a href="https://www.facebook.com/profile.php?id=61553812034072" target="_blank">
+          <a href="https://www.facebook.com/profile.php?id=61553812034072" target="_blank" rel="noreferrer">
             <FaFacebook className={classes["icon"]} />
           </a>
         </div>
