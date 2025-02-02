8d65e340eb29d81276ed9810c90f5c75690334fb
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2025-02-02 15:43:33 +0100

    :art: mobile text

--- src/Components/Headers/MobileMenu/index.tsx
+++ src/Components/Headers/MobileMenu/index.tsx
@@ -18,58 +18,53 @@ import Logo from "../../Logo";
 
 type IProps = {};
 type IState = {
-  isBurgerMenuOpen: boolean;
+	isBurgerMenuOpen: boolean;
 };
 
 export default class MobileMenu extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      isBurgerMenuOpen: false,
-    };
-    this.toggleBurgerMenu = this.toggleBurgerMenu.bind(this);
-    this.closeBurgerMenu = this.closeBurgerMenu.bind(this);
-  }
+	constructor(props: IProps) {
+		super(props);
+		this.state = {
+			isBurgerMenuOpen: false,
+		};
+		this.toggleBurgerMenu = this.toggleBurgerMenu.bind(this);
+		this.closeBurgerMenu = this.closeBurgerMenu.bind(this);
+	}
 
-  public render() {
-    return (
-      <div className={classes["root"]}>
-        <BurgerMenu isOpen={this.state.isBurgerMenuOpen} onOpenChange={this.toggleBurgerMenu} />
+	public render() {
+		return (
+			<div className={classes["root"]}>
+				<BurgerMenu isOpen={this.state.isBurgerMenuOpen} onOpenChange={this.toggleBurgerMenu} />
 
-        {this.state.isBurgerMenuOpen && <div className={classes["background"]} onClick={this.closeBurgerMenu} />}
+				{this.state.isBurgerMenuOpen && <div className={classes["background"]} onClick={this.closeBurgerMenu} />}
 
-        <div
-          className={classNames(classes["menu-mobile-container"], {
-            [classes["menu-open"]]: this.state.isBurgerMenuOpen,
-          })}
-        >
-          <div className={classes["menu-mobile"]} onClick={this.closeBurgerMenu}>
-            <CustomLink text={EWebsiteLinksNames.WELCOME} to={EWebsiteLinks.WELCOME} className={classes["link"]} />
+				<div
+					className={classNames(classes["menu-mobile-container"], {
+						[classes["menu-open"]]: this.state.isBurgerMenuOpen,
+					})}>
+					<div className={classes["menu-mobile"]} onClick={this.closeBurgerMenu}>
+						<CustomLink text={EWebsiteLinksNames.WELCOME} to={EWebsiteLinks.WELCOME} className={classes["link"]} />
 
-            <CustomLink
-              text={EWebsiteLinksNames.HEALING_BENEFITS}
-              to={EWebsiteLinks.HEALING_BENEFITS}
-              className={classes["link"]}
-            />
+						<CustomLink text={EWebsiteLinksNames.HEALING_BENEFITS} to={EWebsiteLinks.HEALING_BENEFITS} className={classes["link"]} />
 
-            <CustomLink text={EWebsiteLinksNames.ABOUT} to={EWebsiteLinks.ABOUT} className={classes["link"]} />
+						<CustomLink text={EWebsiteLinksNames.ABOUT} to={EWebsiteLinks.ABOUT} className={classes["link"]} />
 
-            <CustomLink text={EWebsiteLinksNames.FAQ} to={EWebsiteLinks.FAQ} className={classes["link"]} />
+						<CustomLink text={EWebsiteLinksNames.FAQ} to={EWebsiteLinks.FAQ} className={classes["link"]} />
 
-            <ContactButton size={EButtonSize.MEDIUM} text="Alors, convaincus ?" />
+						<ContactButton size={EButtonSize.MEDIUM} text="Rejoins-moi pour ton soin" />
 
-            <Logo className={classes["logo"]} transparent />
-          </div>
-        </div>
-      </div>
-    );
-  }
+						<Logo className={classes["logo"]} transparent />
+					</div>
+				</div>
+			</div>
+		);
+	}
 
-  private toggleBurgerMenu(isOpen: boolean) {
-    this.setState({ isBurgerMenuOpen: isOpen });
-  }
+	private toggleBurgerMenu(isOpen: boolean) {
+		this.setState({ isBurgerMenuOpen: isOpen });
+	}
 
-  private closeBurgerMenu() {
-    this.setState({ isBurgerMenuOpen: false });
-  }
+	private closeBurgerMenu() {
+		this.setState({ isBurgerMenuOpen: false });
+	}
 }
